9512ed350b6571cba5de0c056f98c5aeea0ce7cd
raspi_01 <none@example.com>
2025-05-14 15:50:54 +0200

    fix: correct syntax injection in patch_buildozer.sh

--- .github/scripts/patch_buildozer.sh
+++ .github/scripts/patch_buildozer.sh
@@ -4,7 +4,7 @@
 TARGET_FILE=$(find $HOME -type f -path "*/buildozer/targets/android.py" 2>/dev/null | head -n 1)
 
 if [ -f "$TARGET_FILE" ]; then
-  sed -i "/sdkmanager/s/^/echo SKIP; exit 0 # /" "$TARGET_FILE"
+  sed -i "s/^\(.*sdkmanager.*\)$/# \1\necho SKIP\nexit 0/" "$TARGET_FILE"
 else
   echo "android.py not found. Patch skipped."
 fi
