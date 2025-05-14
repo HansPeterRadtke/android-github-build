8d547d9f655e4d506742add2180e410a125d639c
raspi_01 <none@example.com>
2025-05-14 10:44:14 +0200

    fix: inject licenses just before Buildozer run

---
+++ .github/scripts/inject_licenses.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mkdir -p \"$HOME/.buildozer/android/platform/android-sdk/licenses\"
+echo 8933bad161af4178b1185d1a37fbf41ea5269c55 > \"$HOME/.buildozer/android/platform/android-sdk/licenses/android-sdk-license\"
+echo d56f5187479451eabf01fb78af6dfcb131a6481e > \"$HOME/.buildozer/android/platform/android-sdk/licenses/android-sdk-preview-license\"
